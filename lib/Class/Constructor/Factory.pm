9d48cb11de767df4684cc7af4af69c77be665d44
Adriano Ferreira <ferreira@cpan.org>
2007-12-27 18:48:12 +0000

    Import of FERREIRA/Class-Constructor-Factory-0.001 from CPAN.
    gitpan-cpan-distribution: Class-Constructor-Factory
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         FERREIRA/Class-Constructor-Factory-0.001.tar.gz
    gitpan-cpan-author:       FERREIRA
    gitpan-cpan-maturity:     released

---
+++ lib/Class/Constructor/Factory.pm
@@ -0,0 +1,69 @@
+
+package Class::Constructor::Factory;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+use Exporter::Lite;
+BEGIN {
+  our @EXPORT = qw( mk_constructor0 defer lazy );
+}
+
+use Scalar::Defer 0.12 qw( force defer lazy );
+# &defer and &lazy are imported just to be exported
+
+sub mk_constructor0 {
+  my $self = shift;
+  my $params = shift;
+
+  my $class = ref $self || $self;
+  my $sub = $self->make_constructor0($params);
+  my $subname = $class . '::new';
+
+  no strict 'refs';
+  *{$subname} = $sub;
+}
+
+sub _is_deferred {
+  UNIVERSAL::isa( shift, 'Scalar::Defer::Deferred' );
+  # within Scalar::Defer, it could be implemented as
+  # ref(shift) eq DEFER_PACKAGE
+}
+
+
+# from Class::Accessor
+sub new {
+  my($proto, $fields) = @_;
+  my($class) = ref $proto || $proto;
+
+  $fields = {} unless defined $fields;
+
+  # make a copy of $fields.
+  bless {%$fields}, $class;
+}
+
+use SUPER;
+
+sub make_constructor0 {
+  my $self = shift;
+  my $params = shift || {};
+
+  # return a closure
+  return sub {
+    my $self = shift;
+    my $fields = shift;
+    my %f = %{ $fields || {} };
+    while ( my ($k, $v) = each %$params ) {
+      if ( !exists $f{$k} ) {
+        $f{$k} = _is_deferred($v) ? force($v) : $v;
+      }
+    }
+    return $self->super('new')->( $self, \%f );
+
+  };
+}
+
+1;
---
+++ t/01use.t
@@ -0,0 +1,6 @@
+#perl -T 
+
+use Test::More tests => 1;
+BEGIN { use_ok('Class::Constructor::Factory') };
+
+diag( "Testing Class::Constructor::Factory $Class::Constructor::Factory::VERSION, Perl $], $^X" );
